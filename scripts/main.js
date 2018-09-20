68c0695f6e205bdd1d7ada3c5671d5eb762f38e3
Taras Kovbasiuk <taraskovbasyk@gmail.com>
2018-09-20 22:47:45 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
 function myFunction() {
-    var x = document.getElementById("fForm");
+	var x = document.getElementById("fForm");
+	var z= document.getElementById("CashyBot");
 	if (x.style.display === "none") {
 		x.style.display = "block";
+		z.style.display = "none";
 	} else {
 		x.style.display = "none";
+		z.style.display = "block";
 	}
+	
+	
 }
